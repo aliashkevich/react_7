41febc30532bca6f6d685a1bc5f91867613c7242
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-05-09 16:53:42 +0200

    Updates

---
+++ precache-manifest.0d67acea7a8899d1b97746db639ad806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9cbe37ce021f5245ba8cbe15d3da98",
+    "url": "/react_7/index.html"
+  },
+  {
+    "revision": "730dc5a2c0064479283f",
+    "url": "/react_7/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3c8aaa6cd6e6ba71d7f6",
+    "url": "/react_7/static/css/main.90c9bc0b.chunk.css"
+  },
+  {
+    "revision": "730dc5a2c0064479283f",
+    "url": "/react_7/static/js/2.b848fbe6.chunk.js"
+  },
+  {
+    "revision": "3c8aaa6cd6e6ba71d7f6",
+    "url": "/react_7/static/js/main.767c2202.chunk.js"
+  },
+  {
+    "revision": "a646ce0826a37f9ca029",
+    "url": "/react_7/static/js/runtime~main.063c5595.js"
+  }
+]);
